13e38bbdc07c59a8391bd19fd04e278c810d8ae9
Daniel-Musunza <musunzafestus2019@gmail.com>
2023-06-29 10:46:33 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+      <div class="editor-wrapper">
+        <div class="editor-heading">
+          Editor
+        </div>
+
+        <textarea id="editor" type="text" cols={50} rows={20}></textarea>
+      </div>
+      <div class="preview-wrapper">
+        <div class="preview-heading">
+          Preview
+        </div>
+        <div class="preview">
+        <h1> Welcome to my React Markdown Previewer!</h1>
+           <h2> This is a sub-heading...</h2>
+            <p> And here's some other cool stuff:</p>
+            Heres some code, between 2 backticks.
+<br></br>
+           <b> You can also make text bold... whoa!</b>
+            Or italic.
+            Or... <i>wait for it...</i> both!
+            <b><i>And feel free to go crazy crossing stuff out.</i></b>
+<br></br>
+            There's also links, and
+
+          <b>  Block Quotes!</b>
+          <br></br>
+           <p> And if you want to get really crazy, even tables:</p>
+           <br></br>
+           <p> Wild Header	Crazy Header	Another Header?</p>
+            Your content can	be here, and it	can be here....
+            And here.	Okay.	I think we get it.
+            And of course there are lists.
+            Some are bulleted.
+            With different indentation levels.
+            That look like this.
+            And there are numbered lists too.
+            Use just 1s if you want!
+           <p> And last but not least, let's not forget embedded images:</p>
+        </div>
+       
+      </div>
     </div>
   );
 }
