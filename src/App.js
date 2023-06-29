519a170f43bfc3a81bdc38adab03374c167f176c
Daniel-Musunza <musunzafestus2019@gmail.com>
2023-06-29 12:20:01 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,50 +1,58 @@
-
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [markdown, setMarkdown] = useState('');
+  const [markedLoaded, setMarkedLoaded] = useState(false);
+
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://cdnjs.cloudflare.com/ajax/libs/marked/2.0.3/marked.min.js';
+    script.async = true;
+
+    script.onload = () => {
+      setMarkedLoaded(true);
+    };
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  const handleInputChange = (event) => {
+    setMarkdown(event.target.value);
+  };
+
+  const getMarkdownText = () => {
+    if (markedLoaded) {
+      const rawMarkup = window.marked(markdown);
+      return { __html: rawMarkup };
+    }
+    return null;
+  };
+
   return (
     <div className="App">
-    
-      <div class="editor-wrapper">
-        <div class="editor-heading">
-          Editor
-        </div>
-
-        <textarea id="editor" type="text" cols={50} rows={20}></textarea>
+      <div className="editor-wrapper">
+        <div className="editor-heading">Editor</div>
+        <textarea
+          id="editor"
+          value={markdown}
+          onChange={handleInputChange}
+          cols={50}
+          rows={20}
+        ></textarea>
       </div>
-      <div class="preview-wrapper">
-        <div class="preview-heading">
-          Preview
-        </div>
-        <div class="preview">
-        <h1> Welcome to my React Markdown Previewer!</h1>
-           <h2> This is a sub-heading...</h2>
-            <p> And here's some other cool stuff:</p>
-            Heres some code, between 2 backticks.
-<br></br>
-           <b> You can also make text bold... whoa!</b>
-            Or italic.
-            Or... <i>wait for it...</i> both!
-            <b><i>And feel free to go crazy crossing stuff out.</i></b>
-<br></br>
-            There's also links, and
-
-          <b>  Block Quotes!</b>
-          <br></br>
-           <p> And if you want to get really crazy, even tables:</p>
-           <br></br>
-           <p> Wild Header	Crazy Header	Another Header?</p>
-            Your content can	be here, and it	can be here....
-            And here.	Okay.	I think we get it.
-            And of course there are lists.
-            Some are bulleted.
-            With different indentation levels.
-            That look like this.
-            And there are numbered lists too.
-            Use just 1s if you want!
-           <p> And last but not least, let's not forget embedded images:</p>
-        </div>
-       
+      <div className="preview-wrapper">
+        <div className="preview-heading">Preview</div>
+        <div
+          className="preview"
+          id="preview"
+          dangerouslySetInnerHTML={getMarkdownText()}
+        ></div>
+        
       </div>
     </div>
   );
